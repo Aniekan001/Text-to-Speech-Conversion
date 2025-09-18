d93594d80c1602a7cab479c1a0ac23bf491da535
aniekan001 <isaacekpoaniekan@gmail.com>
2025-09-18 14:05:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var speech = new SpeechSynthesisUtterance()
+let voices = []
+let voiceselector = document.querySelector("select")
+
+window.speechSynthesis.onvoiceschanged = () =>{
+    voices = window.speechSynthesis.getVoices()
+    speech.voice = voices[0]
+
+    voices.forEach((voice, i) => (voiceselector.options[i] = new Option(voice.name, i)))
+    voiceselector.addEventListener("change", () => {
+        speech.voice = voices[voiceselector.value]
+    })
+}
+//var button = document.querySelector("button")
+document.querySelector("button").addEventListener("click", ()=>{
+    speech.text = document.querySelector("textarea").value
+    window.speechSynthesis.speak(speech)
+})
